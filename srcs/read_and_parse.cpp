5f6471768d519c1e83f682fcfad381484d858b0b
Loris Sanchez <loris1.sanchez@epitech.eu>
2017-07-20 17:55:04 +0200

    Start parsing

--- srcs/read_and_parse.cpp
+++ srcs/read_and_parse.cpp
@@ -8,6 +8,8 @@ ParseRead::ParseRead(std::istream &cin) {
   std::string		tmp;
   std::stringstream	content;
   
-  while (getline(cin, tmp) && std::strncmp(tmp.c_str(), ";;", 2) != 0)    content << tmp << std::endl;
+  while (getline(cin, tmp)
+	 && std::strncmp(tmp.c_str(), ";;", 2) != 0)
+    content << tmp << std::endl;
   _content = content.str();   
 }
