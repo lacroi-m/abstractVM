c4deeb307b2c419860e99e2f0c15d5982473fbcc
Loris Sanchez <loris1.sanchez@epitech.eu>
2017-07-25 14:41:35 +0200

    Try

--- srcs/parsing.cpp
+++ srcs/parsing.cpp
@@ -71,13 +71,13 @@ void				Parsing::suppress_errors() {
 	exit(84);
       }
     }
-    else
+    /*    else
       if (std::distance(it->second.begin(), it->second.end()) > 1) {
 	std::cerr << "Error at " << it->first
 		  << " with the command " << it->second[0]
 		  << std::endl;
 	exit(84);
-      }
+	}*/
   }
   auto it = _clean_map.end();
   if (it == _clean_map.begin())
